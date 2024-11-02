fab1faec25db94d60155955655c236e78d724147
Facundo Godoy <fcndgodoy@gmail.com>
2024-11-02 18:58:33 -0300

    LoginController

---
+++ WebApi_TransporteSanchez/JwtConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi_TransporteSanchez
+{
+    public static class JwtConfig
+    {
+
+      public const string SecretKey = "7aqmj&/Sk)l)FOJUN3f5"; // Cambia esto por una clave segura y larga.
+      public const int ExpirationMinutes = 30; // Tiempo de expiración del token en minutos.
+
+    }
+}
--- WebApi_TransporteSanchez/SGTL.Designer.cs
+++ WebApi_TransporteSanchez/SGTL.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\Macena\Source\Repos\SGTL\WebApi_TransporteSanchez\SGTL.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\Macena\source\repos\SGTL\WebApi_TransporteSanchez\SGTL.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
